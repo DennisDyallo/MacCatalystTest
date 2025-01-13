298f3bdd7566b2094a3e39888e15d33111126af0
Dennis Dyall <dennis.dyall@yubico.com>
2025-01-13 13:53:47 +0100

    Add project files.

---
+++ MacCatalystTest/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MacCatalystTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            TestLabel.Text = $"OS: "+ OperatingSystem;
+        }
+
+        public static SdkPlatform OperatingSystem
+        {
+            get
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    return SdkPlatform.Windows;
+                }
+
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    return SdkPlatform.MacOS;
+                }
+
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Create("MACCATALYST")))
+                {
+                    return SdkPlatform.MacCatalyst;
+                }
+
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    return SdkPlatform.Linux;
+                }
+
+                return SdkPlatform.Unknown;
+            }
+        }
+
+        public enum SdkPlatform
+        {
+            Unknown,
+            Windows,
+            MacOS,
+            MacCatalyst,
+            Linux
+        }
+    }
+}
